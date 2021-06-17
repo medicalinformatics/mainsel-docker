48ecc16a91dc4c14db43c69f654fa6a0e73b0155
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2021-06-17 12:49:37 +0200

    refactor: Removed everything thats no longer needed

--- config/db/scripts/create-user.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE mainzelliste;
-CREATE ROLE mainzelliste WITH PASSWORD 'mainzelliste';
-GRANT ALL PRIVILEGES ON DATABASE mainzelliste TO mainzelliste;
-ALTER ROLE mainzelliste WITH LOGIN;
--- scripts/transformCsv.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-echo "nachname;vorname;geburtsname;geburtsjahr;geburtsmonat;geburtstag;ort;plz"
-awk -F ";" 'NR>2 {split($7,array,"-");print $3 FS $4 FS $5 FS array[1] FS array[2] FS array[3] FS $8 FS $9 }' $1
