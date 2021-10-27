2a08af4a914704a98d158f015435f14623b287a9
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2021-10-27 15:45:45 +0200

    Adjusted Scripts to use Docker Compose Exec
    Docker Exec caused Issues with newer docker-compose versions

--- get-status.sh
+++ get-status.sh
@@ -2,4 +2,5 @@
 set -euo pipefail;
 SITE=${1:-anjou}
 echo "Info: Requesting current status of jobs on $SITE";
-docker exec "$SITE"_mainzelliste_1 curl -s http://secureepilinker:8161/jobs/list;
+docker-compose -p ${SITE} exec mainzelliste curl http://secureepilinker:8161/jobs/list
+# docker run --rm --name mainsel-get-status --network ${SITE}_intranet mwendler/wget -q -O - --no-proxy http://secureepilinker:8161/jobs/list;
--- init-match.sh
+++ init-match.sh
@@ -3,4 +3,5 @@ set -euo pipefail;
 SITE=${1:-anjou}
 REMOTE_SITE=${2:-bapu}
 echo "Info: Initiating matching between ${SITE} and ${REMOTE_SITE}"
-docker exec ${SITE}_mainzelliste_1 wget http://localhost:8080/Communicator/matchMN/trigger/${REMOTE_SITE}
+docker-compose -p ${SITE} exec mainzelliste curl -s http://localhost:8080/Communicator/matchMN/trigger/${REMOTE_SITE}
+# docker run --rm --name mainsel-init-match --network ${SITE}_intranet mwendler/wget --no-proxy http://mainzelliste:8080/Communicator/matchMN/trigger/${REMOTE_SITE}
